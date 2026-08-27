ffbf585a4bc65a4a615b85c6b4f46962c2f4c32a
IEatSpaceRocks <149251510+IEatSpaceRocks@users.noreply.github.com>
2026-08-27 18:10:54 +0200

    v1.2
    Cleaned up code, added comments.

--- snake-terminal.py
+++ snake-terminal.py
@@ -1,21 +1,33 @@
 # IEatSpaceRocks, 27/08/2026
 
+# Libraries
 import random
 import os, subprocess
 
+# Generate board
 board = [["g"] * 17 for _ in range(15)]
 
+# Variables
 snakeY, snakeX = 7, 8
 running = True
-    
+
+
+# Functions
+
 def clearTerm():
     subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
     
+    
+# MAIN LOOP
+    
 while running:
+    
     clearTerm()
     
+    # Add snake to board
     board[snakeY][snakeX] = "S"
     
+    # Print board (Every second grass patch will be a different shade)
     count = 0
     for y in range(15):
         for x in range(17):
@@ -29,13 +41,15 @@ while running:
             count += 1
         print()
         
+    # Remove previous snake
     board[snakeY][snakeX] = "g"
     
+    # Input for movement direction
     move = input()
-    
     while move not in ("w", "a", "s", "d"):
         move = input()
-        
+    
+    # Logic for movement
     if move == "w":
         snakeY -= 1
     elif move == "a":
@@ -43,10 +57,10 @@ while running:
     elif move == "s":
         snakeY += 1
     else:
-        snakeX += 1 
-    
+        snakeX += 1
+        
+    # Check if player is out of bounds (Dead)
     if snakeY < 0 or snakeX < 0 or snakeY > 14 or snakeX > 16:
-        print("You die")
+        print("You died")
         running = False
-    else:
-        board[snakeY][snakeX] = "S"
+        
