87ab725e384be1d60f48877735e4bddce0ae7ec7
IEatSpaceRocks <149251510+IEatSpaceRocks@users.noreply.github.com>
2026-08-28 10:41:00 +0200

    v2.0
    Added a coloured board built upon the letter based framework. Also more documentation.
    Known issues: board flickering!
    Need to implement: Apple, Snake getting longer, automatic snake movement every frame, scoreboard, borders

--- snake-terminal.py
+++ snake-terminal.py
@@ -1,4 +1,4 @@
-# IEatSpaceRocks, 27/08/2026
+# IEatSpaceRocks, 28/08/2026
 
 # Libraries
 import random
@@ -12,10 +12,47 @@ snakeY, snakeX = 7, 8
 running = True
 
 
+# Set up ANSI text formatting, define all colours used
+class ANSI:
+    RESET = "\x1b[0m"
+    
+    def getCol(rgb):
+        r, g, b = rgb
+        return f"\x1b[48;2;{r};{g};{b}m"
+    
+    GREEN1 = getCol((167, 217, 72))
+    GREEN2 = getCol((142, 204, 57))
+    BLUE = getCol((66, 111, 227))
+
+
 # Functions
 
+# Clear the terminal
 def clearTerm():
     subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+
+# Print a 4 space long block in a given colour
+def printBlock(colour):
+    print(getattr(ANSI, colour) + "    " + ANSI.RESET, end="")
+
+# Print the board itself
+def printBoard():
+    count = 0                                       # Count determines if the given patch of grass is lighter or darker green
+    for y in range(15):                             # For the 15 rows
+        for _ in range(2):                          # So that each block of 4 spaces gets generated under eachother, to make a full patch, that is 4 spaces wide and 2 high
+            for x in range(17):                     # For the 17 columns
+                if board[y][x] == "g":              # If a grass tile:
+                    if count % 2 == 0:              # If count is even -> light green
+                        printBlock("GREEN1")
+                    else:                           # If count is uneven -> darker green
+                        printBlock("GREEN2")
+                elif board[y][x] == "S":            # If a snake tile:
+                    printBlock("BLUE")
+                count += 1                          # Add one to count, to change the grass colour
+            count += 1                              # Add one to count after each row, to make it print the same colour blocks again (So we get a 2 high patch)
+            print()                                 # New line
+        count += 1                                  # Add one to count, so after every 2 rows of same colours, we get an offset 2 rows
+                
     
     
 # MAIN LOOP
@@ -28,19 +65,8 @@ while running:
     board[snakeY][snakeX] = "S"
     
     # Print board (Every second grass patch will be a different shade)
-    count = 0
-    for y in range(15):
-        for x in range(17):
-            if board[y][x] == "g":
-                if count % 2 == 0:
-                    print("g", end=" ")
-                else:
-                    print("G", end=" ")
-            elif board[y][x] == "S":
-                print("S", end=" ")
-            count += 1
-        print()
-        
+    printBoard()
+    
     # Remove previous snake
     board[snakeY][snakeX] = "g"
     
